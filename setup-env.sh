eba54b6c7aa444130c03c7d50d66972ac6981ea3
Arunas Grigalionis <arunas.grigalionis@gmail.com>
2024-04-03 21:34:43 +0300

    Initial commit

---
+++ setup-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ -e /etc/redhat-release ]
+then
+  sudo dnf -y install python3.9 python3.9-pip binutils make
+fi
+if [ -e /etc/debian_version ]
+then
+	sudo apt update
+  sudo apt install -y git make binutils python3-venv python3-stdeb fakeroot python3-all dh-python
+fi
+poetry env use python3.9
+poetry install
+make dev
